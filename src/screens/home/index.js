2157ba68c892379447c569e9a9c518fd6a87d21e
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-05 09:08:50 -0300

    feat: add a example.env

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -14,23 +14,29 @@ const HomeScreen = () => (
           <Column size={5}>
             <Title size={2} spaced className="has-text-white">
               Create notes easily and access when you wants on the cloud
-</Title>
+            </Title>
             <Title size={5} spaced className="has-text-light" subtitle>
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.<br /><br />
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print.
-</Title>
-            <Link to='/register' className="button is-outlined is-white is-large">
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs.
+              <br />
+              <br />
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print.
+            </Title>
+            <Link
+              to="/register"
+              className="button is-outlined is-white is-large"
+            >
               <strong>Register for free Now</strong>
             </Link>
           </Column>
           <Column size={6} offset={1}>
-            <img src={presentationImage}/>
+            <img src={presentationImage} />
           </Column>
         </Column.Group>
       </Container>
     </Section>
-
   </Fragment>
-)
+);
 
 export default HomeScreen;
